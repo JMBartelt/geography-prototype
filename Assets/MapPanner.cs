4e7c6205e9d1b07dee0f087cb41253bbf6b85bd6
J.M. Bartelt <jm@jmbartelt.com>
2023-12-02 17:42:05 -0500

    trackpad

--- Assets/MapPanner.cs
+++ Assets/MapPanner.cs
@@ -7,13 +7,18 @@ using UnityEngine;
 public class MapPanner : MonoBehaviour
 {
     [SerializeField] private MapRenderer _mapRenderer;
-    [SerializeField] private float panSpeed = 0.01f;
+    [SerializeField] private float panSpeed = 0.001f;
 
     private Vector2 _panDirection = Vector2.zero;
 
     public void SetPan(Vector2 direction)
     {
         _panDirection = direction;
+    }
+
+    public void StopPan()
+    {
+        _panDirection = Vector2.zero;
     }
     void Update()
     {
@@ -22,7 +27,7 @@ public class MapPanner : MonoBehaviour
 
     public void Pan(Vector2 direction)
     {
-        LatLon newCenter = new LatLon(_mapRenderer.Center.LatitudeInDegrees + direction.y*panSpeed, _mapRenderer.Center.LongitudeInDegrees + direction.x*panSpeed);
+        LatLon newCenter = new LatLon(_mapRenderer.Center.LatitudeInDegrees + direction.y*panSpeed*(50f/_mapRenderer.ZoomLevel*_mapRenderer.ZoomLevel), _mapRenderer.Center.LongitudeInDegrees + direction.x*panSpeed*(50f/_mapRenderer.ZoomLevel*_mapRenderer.ZoomLevel));
         _mapRenderer.Center = newCenter;
     }
 
--- Assets/PokeTrackpad.cs
+++ Assets/PokeTrackpad.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Oculus.Interaction;
+using UnityEngine.Events;
 
 public class PokeTrackpad : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private InteractableUnityEventWrapper interactableUnityEventWrapper;
+    [SerializeField] private Transform _leftFingertip;
+    [SerializeField] private Transform _trackpad;
+    [SerializeField] private UnityEvent<Vector2> trackpadEvent;
+    [SerializeField] private UnityEvent onTrackpadRelease;
+
+    private bool touching = false;
     void Start()
     {
-        
+        interactableUnityEventWrapper.WhenSelect.AddListener(OnSelect);
+        interactableUnityEventWrapper.WhenUnselect.AddListener(OnUnselect);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(touching)
+        {
+            // Get fingertip position
+            Vector3 fingertipPosition = _leftFingertip.position;
+            // Get trackpad position
+            Vector3 trackpadPosition = _trackpad.position;
+            // Get vector from fingertip to trackpad
+            Vector3 vector = fingertipPosition - trackpadPosition;
+            // take account of size of trackpad
+            Vector3 trackpadSize = _trackpad.localScale;
+            vector.x /= trackpadSize.x;
+            vector.z /= trackpadSize.y;
+
+            // Send vector to event
+            trackpadEvent.Invoke(new Vector2(vector.x, vector.z));
+            Debug.Log("Vector: " + vector);
+        }
     }
+
+    private void OnSelect()
+    {
+        touching = true;
+    }
+
+    private void OnUnselect()
+    {
+        touching = false;
+        onTrackpadRelease.Invoke();
+    }
+
+
 }
